6b03e0df3dd98f043840720f6444f4558dc06870
Asher Khan <muhammad.khan@ctx.edu>
2018-11-17 13:47:52 -0500

    Asher Khan PA6.1

--- pa6-1.s
+++ pa6-1.s
@@ -1,3 +1,9 @@
+@Asher Khan
+@PA6-1
+@11/09/18
+@this program will compute a^b - 3a by using the function pow
+
+
 		@---------------------
 		@	Programming Assignment 6, Problem 1
 		@	Your required header information goes here
@@ -8,9 +14,17 @@
 		@ The result should be in R4 after the program completes
 	.extern printf
 	.global	main
-	.func main	
+	.func main
 main:	PUSH {LR}
-	@	Your code goes here
+	LDR R0, =a
+	LDR R1, =b
+	LDR R0, [R0]
+	LDR R1, [R1]
+    MOV R3, #3
+    MUL R3, R0, R3
+    BL pow
+	SUB R4, R0, R3
+
 
 
 	@ Code to print your answer.  DO NOT CHANGE!
@@ -18,7 +32,7 @@ main:	PUSH {LR}
 	MOV	R1, R4
 	BL 	printf
 	POP	{PC}
-		
+
 	@---------------------
 	@  pow function.  DO NOT CHANGE!
 	@---------------------
@@ -28,8 +42,8 @@ pow:	PUSH		{LR}
 	MOV		R5, #1
 loop:	CMP		R4, #0
 	BEQ		exit
-	MUL		R5, R0, R5
-	SUB		R4, R4, #1
+	MUL		R5, R0, R5      @R5 is #1 and R0 is a(#3)
+	SUB		R4, R4, #1      @ sub R4 and 1 for the loop
 	BAL		loop
 exit:	MOV		R0, R5
 	POP		{R4-R6}
